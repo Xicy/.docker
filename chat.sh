fe63c01fb63d5b1a07803966d45da3e1100a5f2a
Umut Akkaya <Xicy@users.noreply.github.com>
2018-12-15 18:21:51 +0300

    Update chat.sh

--- chat.sh
+++ chat.sh
@@ -2,5 +2,6 @@ export $(grep -v '^#' .env | xargs)
 docker pull rocketchat/rocket.chat:latest
 docker stop rocketchat
 docker rm rocketchat
-docker run --name=rocketchat --env="ROOT_URL=https://chat.$DOMAIN" --env="VIRTUAL_HOST=chat.$DOMAIN" --env="VIRTUAL_PORT=3000" --network=nginx-net --restart=unless-stopped --link mongo -d rocketchat/rocket.chat
+docker run --name=rocketmongo mongo
+docker run --name=rocketchat --env="ROOT_URL=https://chat.$DOMAIN" --env="VIRTUAL_HOST=chat.$DOMAIN" --env="VIRTUAL_PORT=3000" --network=nginx-net --restart=unless-stopped --link rocketmongo -d rocketchat/rocket.chat
 unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs)
